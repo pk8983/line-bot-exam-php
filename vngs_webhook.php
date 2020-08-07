4e7fca0af7bcfe70d3722af41c19bf65a935dfd3
mosadmin02 <69343035+mosadmin02@users.noreply.github.com>
2020-08-07 15:49:53 +0700

    Update vngs_webhook.php

--- vngs_webhook.php
+++ vngs_webhook.php
@@ -35,7 +35,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text." ".$result_profile['displayName'];
+				'text' => $text." ".$result_profile['displayName'],
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
