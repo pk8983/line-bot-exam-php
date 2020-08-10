6268d11e1158101e8232a2604dadecea2ce7ab56
mosadmin02 <69343035+mosadmin02@users.noreply.github.com>
2020-08-10 13:50:51 +0700

    Update r-my-vngs.php

--- r-my-vngs.php
+++ r-my-vngs.php
@@ -1,6 +1,8 @@
 <?php // callback.php
 
 require "vendor/autoload.php";
+require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+
 $access_token = 'yFTYt7Fkd+vdapetkarW7GmU7ocMFRfAVEgjHgYozfza8hV0N9MPK8on4s7ekZmhLoPXwgaCaPOW6Lh/cd6kDM3fy9BISv3U8CWiUmiEM0yaddwhe2Sp3g4j5jqKiPSONi8dw5E5RpRQIfsj3FVw2QdB04t89/1O/w1cDnyilFU=';
 $channelSecret = "d63194425ca914f580959b2d17c0fc15";
 // Get POST body content
