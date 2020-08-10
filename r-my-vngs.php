d684d5adfdeda839334ce5a2c1286306bd5b1081
mosadmin02 <69343035+mosadmin02@users.noreply.github.com>
2020-08-10 14:05:55 +0700

    Update r-my-vngs.php

--- r-my-vngs.php
+++ r-my-vngs.php
@@ -5,12 +5,13 @@ require "vendor/autoload.php";
 $access_token = 'yFTYt7Fkd+vdapetkarW7GmU7ocMFRfAVEgjHgYozfza8hV0N9MPK8on4s7ekZmhLoPXwgaCaPOW6Lh/cd6kDM3fy9BISv3U8CWiUmiEM0yaddwhe2Sp3g4j5jqKiPSONi8dw5E5RpRQIfsj3FVw2QdB04t89/1O/w1cDnyilFU=';
 $channelSecret = "d63194425ca914f580959b2d17c0fc15";
 $idPush = 'U0e6fd4dbfdaf9d6114d836617c0c26a0';
-$message = "PR ของท่านได้รับการอนุมัติ";//$_POST['message'];
-	// Loop through each event
-
+ 	$message = "PR ของท่านได้รับการอนุมัติ";//$_POST['message'];
+// Parse JSON
+// Validate parsed JSON data
 	
+
 		$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-		$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+		$bot = new  \LINE\LINEBot($httpClient, array('channelSecret' => $channelSecret));
 		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
 		$response = $bot->pushMessage($idPush, $textMessageBuilder);
 
