6fad1e5eaf8a3beac63498599ff7c40f271d536b
mosadmin02 <69343035+mosadmin02@users.noreply.github.com>
2020-08-10 13:53:18 +0700

    Update r-my-vngs.php

--- r-my-vngs.php
+++ r-my-vngs.php
@@ -1,25 +1,14 @@
 <?php // callback.php
 
 require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = 'yFTYt7Fkd+vdapetkarW7GmU7ocMFRfAVEgjHgYozfza8hV0N9MPK8on4s7ekZmhLoPXwgaCaPOW6Lh/cd6kDM3fy9BISv3U8CWiUmiEM0yaddwhe2Sp3g4j5jqKiPSONi8dw5E5RpRQIfsj3FVw2QdB04t89/1O/w1cDnyilFU=';
 $channelSecret = "d63194425ca914f580959b2d17c0fc15";
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new  \LINE\LINEBot($httpClient, array('channelSecret' => $channelSecret));
-	$user_id = "U0e6fd4dbfdaf9d6114d836617c0c26a0";//$_POST['user_id'];
- 	$message = "PR ของท่านได้รับการอนุมัติ";//$_POST['message'];
+$idPush = 'U0e6fd4dbfdaf9d6114d836617c0c26a0';
+$message = "PR ของท่านได้รับการอนุมัติ";//$_POST['message'];
 	// Loop through each event
 
-		// Reply only when message sent is in 'text' format
-			// Get text sent
-			$text = $user_id;
-			// Get replyToken
+	
 
 		$idPush = 'U0e6fd4dbfdaf9d6114d836617c0c26a0'
 		$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
