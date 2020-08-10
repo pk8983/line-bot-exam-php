cf7de7ed8e247b35aa2876b0a0b8bbaf19fedef5
mosadmin02 <69343035+mosadmin02@users.noreply.github.com>
2020-08-10 13:49:43 +0700

    Update r-my-vngs.php

--- r-my-vngs.php
+++ r-my-vngs.php
@@ -1,8 +1,6 @@
 <?php // callback.php
 
 require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-
 $access_token = 'yFTYt7Fkd+vdapetkarW7GmU7ocMFRfAVEgjHgYozfza8hV0N9MPK8on4s7ekZmhLoPXwgaCaPOW6Lh/cd6kDM3fy9BISv3U8CWiUmiEM0yaddwhe2Sp3g4j5jqKiPSONi8dw5E5RpRQIfsj3FVw2QdB04t89/1O/w1cDnyilFU=';
 $channelSecret = "d63194425ca914f580959b2d17c0fc15";
 // Get POST body content
@@ -21,35 +19,15 @@ $bot = new  \LINE\LINEBot($httpClient, array('channelSecret' => $channelSecret))
 			$text = $user_id;
 			// Get replyToken
 
-			
+		$idPush = 'U0e6fd4dbfdaf9d6114d836617c0c26a0'
+		$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+		$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+		$response = $bot->pushMessage($idPush, $textMessageBuilder);
+
+		echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $message,
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'userId' => $user_id,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		
 	
 
 ?>
