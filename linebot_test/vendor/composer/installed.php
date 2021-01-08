cb9e8abef03d620c3fae80d8c78e1e9e6ce32e28
pk8983 <52188121+pk8983@users.noreply.github.com>
2021-01-08 17:04:58 +0700

    Add files via upload

---
+++ linebot_test/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'linecorp/line-bot-sdk' => 
+    array (
+      'pretty_version' => '6.1.0',
+      'version' => '6.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a3d3385aabed6e53071766587491d6e7fd2021e8',
+    ),
+  ),
+);
