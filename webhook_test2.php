e01d3e3ccc5a2d20ddb0342cd4ec65c319e914d4
pk8983 <52188121+pk8983@users.noreply.github.com>
2021-01-08 15:42:03 +0700

    Update webhook_test2.php

--- webhook_test2.php
+++ webhook_test2.php
@@ -5,6 +5,10 @@ define('LINE_MESSAGE_CHANNEL_SECRET','2ec65e2ccf14cdb95e59c30c90640cbb');
 define('LINE_MESSAGE_ACCESS_TOKEN','DW8c1hq6M+RQ9/paVgF5sdpLmAYH1QDM14rCwiMXtse1t5JnXKSmc+F5ecquehzuXsbKO7uEGXsXgI/B+pvF7uUVkc2FB5RRf0Xdd5lQGhthiTe2b5Pin5EQjQfxVgKrJf2IJtPQKRJtm3PCkuo34AdB04t89/1O/w1cDnyilFU=
 ');
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require "vendor/autoload.php";
 //require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
