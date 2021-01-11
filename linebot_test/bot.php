9fcf066132f401f14b80b07596d7da36ea6f4eff
pk8983 <52188121+pk8983@users.noreply.github.com>
2021-01-11 15:13:30 +0700

    Update bot.php

--- linebot_test/bot.php
+++ linebot_test/bot.php
@@ -1,8 +1,8 @@
 <?php
 // กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
  
 // include composer autoload
 require_once 'vendor/autoload.php';
