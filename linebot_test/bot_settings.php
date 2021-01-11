b46954d8335c561a0712b328d8dd340ace333cf7
pk8983 <52188121+pk8983@users.noreply.github.com>
2021-01-11 14:53:25 +0700

    Update bot_settings.php

--- linebot_test/bot_settings.php
+++ linebot_test/bot_settings.php
@@ -1,6 +1,7 @@
 <?php
 /// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
-define('LINE_MESSAGE_CHANNEL_ID','กรอก ค่า Channel ID');
-define('LINE_MESSAGE_CHANNEL_SECRET','กรอกค่า Channel secret');
-define('LINE_MESSAGE_ACCESS_TOKEN','กรอกค่า Channel access token');
+define('LINE_MESSAGE_CHANNEL_ID','1655560073');
+define('LINE_MESSAGE_CHANNEL_SECRET','2ec65e2ccf14cdb95e59c30c90640cbb');
+define('LINE_MESSAGE_ACCESS_TOKEN','DW8c1hq6M+RQ9/paVgF5sdpLmAYH1QDM14rCwiMXtse1t5JnXKSmc+F5ecquehzuXsbKO7uEGXsXgI/B+pvF7uUVkc2FB5RRf0Xdd5lQGhthiTe2b5Pin5EQjQfxVgKrJf2IJtPQKRJtm3PCkuo34AdB04t89/1O/w1cDnyilFU=
+');
 ?>
